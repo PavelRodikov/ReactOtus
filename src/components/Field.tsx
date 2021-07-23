a95d191567feec812985144d984a7a50c3b1657a
PavelRodikov <Pavel.A.Rodikov@SZ.RT.RU>
2021-07-23 10:40:38 +0300

    delete unused import

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,29 +1,11 @@
 import React, {FC} from "react";
 import {Cell} from "./Cell";
-import Repeat from 'react-repeat-component';
 
 export interface FieldProps{
     countCell : number
 }
 
 export const Field : FC<FieldProps> = ({countCell}) =>
-    <div>
-        <table>
-            <tbody>
-            {[...Array(countCell)].map((item, indexRow) =>
-                <tr key={indexRow}>
-                    {[...Array(countCell)].map((item, indexCell) =>
-                        <td key={indexCell}>
-                            <Cell coordinates={[indexCell,indexRow]} onClick={(coordinates)=>console.log(coordinates)} />
-                        </td>
-                    )}
-                </tr>
-            )}
-            </tbody>
-        </table>
-    </div>
-
-export const FieldNew : FC<FieldProps> = ({countCell}) =>
     <div>
         <table>
             <tbody>
