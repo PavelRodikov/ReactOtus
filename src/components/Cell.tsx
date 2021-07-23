361185bfca3a689067312d1e185e61c25977753f
PavelRodikov <Pavel.A.Rodikov@SZ.RT.RU>
2021-07-23 15:26:42 +0300

    add cell number

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,16 +1,16 @@
 import React, {FC} from "react";
 import styled from '@emotion/styled';
 
-export type Coords = number[];
+export type Coords = [number,number];
 
 export interface CellProps{
     coordinates : Coords,
     onClick : (coordinates: Coords) => void;
 }
 
-export const Cell : FC<CellProps> = ({coordinates,onClick}) =>
+export const Cell : FC<CellProps> = ({coordinates,onClick,children}) =>
     <button
         onClick={()=>onClick(coordinates)}
     >
-        {coordinates[0]}
+        {children}
     </button>;
