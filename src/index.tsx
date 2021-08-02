9524ca31ff379313afa7613fef3bbaaf689db854
PavelRodikov <Pavel.A.Rodikov@SZ.RT.RU>
2021-08-02 13:33:56 +0300

    remove unused code

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,8 @@
-import React, { FC } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import {Field} from "./components/Field";
-/*
-interface FullNameProps{
-    name : string;
-    surname : string;
-    showSurname? : boolean;
-}
 
-export interface LevelProps{
-    optionNames:string[];
-}
 
-const FullName: FC<FullNameProps> = (props) => <h2>{props.name} {props.showSurname && props.surname}</h2>;
-
-export const Select: FC<LevelProps> = ({optionNames}) => (
-    <select >
-        {optionNames.map((item:string) => (
-            <option key={item} value={item}>
-                {item}
-            </option>
-            )
-        )}
-    </select>
-);
-
-const HelloWorld = () => (
-    <div>
-        <h1>Hello <FullName showSurname name="Pavel" surname="Rodikov"/>!</h1>
-        <Select optionNames={["admin","user","maintainer"]}></Select>
-    </div>
-
-);
-*/
 const MyField = () => (
     <Field countCell={5}></Field>
 )
